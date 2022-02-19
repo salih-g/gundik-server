390736af593b8439cc72688d414fdc7b1e2f5138
Salih Gumus <sudanmerinosu@gmail.com>
2022-02-19 16:16:56 +0300

    add socket.io

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 
 exports.MONGO_URL = process.env.MONGO_URL;
+exports.CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:8080';
 exports.PORT = process.env.PORT || 8081;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+const socketIO = require('socket.io');
+
 const logger = require('./logger');
 const server = require('./server');
-const { PORT } = require('./config');
+const { PORT, CLIENT_URL } = require('./config');
+
+const io = socketIO(server, {
+	cors: {
+		origin: CLIENT_URL,
+		methods: ['GET', 'POST'],
+		credentials: true,
+	},
+	allowEIO3: true,
+});
 
 process.on('unhandledRejection', (reason, p) =>
 	logger.error('Unhandled Rejection at: Promise ', p, reason),
@@ -9,3 +20,20 @@ process.on('unhandledRejection', (reason, p) =>
 server.on('listening', () =>
 	logger.info(`App started on:http://localhost:${PORT}`),
 );
+
+io.on('connection', (socket) => {
+	console.log(socket.id);
+
+	// socket.on('pause', () => {
+	// 	socket.broadcast.to(socket.curRoom).emit('pause');
+	// });
+	// socket.on('play', () => {
+	// 	socket.broadcast.to(socket.curRoom).emit('play');
+	// });
+	// socket.on('video id', (videoId) => {
+	// 	socket.broadcast.to(socket.curRoom).emit('video id', videoId);
+	// });
+	// socket.on('timestamp', (timestamp) => {
+	// 	socket.broadcast.to(socket.curRoom).emit('timestamp', timestamp);
+	// });
+});
